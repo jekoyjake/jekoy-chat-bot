03fa36dae5ee7f1b3a0b16038cc7fe2671429412
Jake Bardinas <39621990+jekoyjake@users.noreply.github.com>
2023-03-04 02:27:19 +0800

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -404,7 +404,7 @@ class ChatBot(Client):
                               thread_id=thread_id, thread_type=ThreadType.USER)
 
         def chatGPT(self, query):
-            openai.api_key = "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"
+            openai.api_key = "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"
 
             response = openai.Completion.create(
                 model="text-davinci-003",
